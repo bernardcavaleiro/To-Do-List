62f17ed50411be1a8e9ef462017a246ac5182195
Bernardo Cavaleiro <bernardcavaleiro@gmail.com>
2021-11-22 14:27:23 -0300

    Test

---
+++ functions.js
@@ -0,0 +1,35 @@
+document.querySelector('#push').onclick = function(){
+    if(document.querySelector("#newtask input") .value.length == 0){
+        alert("Não se esqueça de preencher uma tarefa.")
+    }
+    ///////////////////////////////////////////////////////////////////
+    else{
+        document.querySelector('#tasks').innerHTML
+        += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector
+                        ('#newtask input').value}
+                </span>
+                <button class="delete">
+                <i class="far fa-trash-alt"></i>
+                </button>
+            </div>
+        `;
+        var current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length;  i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+
+        var tasks =  document.querySelectorAll(".task");
+        for(var i=0; i<tasks.length; i++){
+            tasks[i].onclick = function(){
+                this.classList.toggle('completed');
+            }
+        }
+
+        document.querySelector("#newtak input"). value = "";
+    }
+}
