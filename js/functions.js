a0807b1fefc858f4edc049b86f72ac1df3efdfa4
ianmateusrodd <mateusroddi@gmail.com>
2022-03-17 22:30:17 -0300

    send with enter key

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,13 @@
+document.addEventListener("keypress", function(e) {
+    if(e.key === 'Enter') {
+    
+        var btn = document.querySelector("#push");
+      
+        btn.click();
+        
+    }
+  })
+
 document.querySelector('#push').onclick = function(){
     if(document.querySelector("#newtask input") .value.length == 0){
         alert("Não se esqueça de preencher uma tarefa.")
@@ -7,10 +17,10 @@ document.querySelector('#push').onclick = function(){
         document.querySelector('#tasks').innerHTML
         += `
             <div class="task">
-                <span id="taskname">
+                <p id="taskname">
                     ${document.querySelector
                         ('#newtask input').value}
-                </span>
+                </p>
                 <button class="delete">
                 <i class="far fa-trash-alt"></i>
                 </button>
@@ -22,14 +32,13 @@ document.querySelector('#push').onclick = function(){
                 this.parentNode.remove();
             }
         }
-
         var tasks =  document.querySelectorAll(".task");
         for(var i=0; i<tasks.length; i++){
             tasks[i].onclick = function(){
-                this.classList.toggle('completed');
+                this.classList.toggle('completed');                
             }
-        }
+        }  
 
-        document.querySelector("#newtak input"). value = "";
+        document.querySelector("#newtask input").value = "";
     }
 }
